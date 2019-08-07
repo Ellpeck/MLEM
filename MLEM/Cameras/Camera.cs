2ead7c6c6be50f4a022d791885205ff4ae32b12f
Ellpeck <megamaximal@gmail.com>
2019-08-07 16:32:19 +0200

    border constraints as well as some other useful goodies

--- MLEM/Cameras/Camera.cs
+++ MLEM/Cameras/Camera.cs
@@ -15,8 +15,16 @@ namespace MLEM.Cameras {
                 return Matrix.CreateScale(this.Scale, this.Scale, 1) * Matrix.CreateTranslation(new Vector3(pos, 0));
             }
         }
-        public Vector2 LookingPosition => this.Position + new Vector2(this.Viewport.Width / 2, this.Viewport.Height / 2) / this.Scale;
+        public Vector2 Max {
+            get => this.Position + this.ScaledViewport;
+            set => this.Position = value - this.ScaledViewport;
+        }
+        public Vector2 LookingPosition {
+            get => this.Position + this.ScaledViewport / 2;
+            set => this.Position = value - this.ScaledViewport / 2;
+        }
         public Viewport Viewport => this.graphicsDevice.Viewport;
+        public Vector2 ScaledViewport => new Vector2(this.Viewport.Width, this.Viewport.Height) / this.Scale;
 
         private readonly bool roundPosition;
         private readonly GraphicsDevice graphicsDevice;
@@ -24,10 +32,6 @@ namespace MLEM.Cameras {
         public Camera(GraphicsDevice graphicsDevice, bool roundPosition = true) {
             this.graphicsDevice = graphicsDevice;
             this.roundPosition = roundPosition;
-        }
-
-        public void LookAt(Vector2 worldPos) {
-            this.Position = worldPos - new Vector2(this.Viewport.Width / 2, this.Viewport.Height / 2) / this.Scale;
         }
 
         public Vector2 ToWorldPos(Vector2 pos) {
@@ -40,6 +44,18 @@ namespace MLEM.Cameras {
 
         public (Vector2 topLeft, Vector2 bottomRight) GetVisibleArea() {
             return (this.ToWorldPos(Vector2.Zero), this.ToWorldPos(new Vector2(this.Viewport.Width, this.Viewport.Height)));
+        }
+
+        public void ConstrainWorldBounds(Vector2 min, Vector2 max) {
+            if (this.Position.X < min.X)
+                this.Position.X = min.X;
+            if (this.Position.Y < min.Y)
+                this.Position.Y = min.Y;
+
+            if (this.Max.X > max.X)
+                this.Max = new Vector2(max.X, this.Max.Y);
+            if (this.Max.Y > max.Y)
+                this.Max = new Vector2(this.Max.X, max.Y);
         }
 
     }
