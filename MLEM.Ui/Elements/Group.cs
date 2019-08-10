519ac01b9eec497dc14d789a8120bdb992fc6d9f
Ellpeck <megamaximal@gmail.com>
2019-08-10 13:44:48 +0200

    added an empty group element

---
+++ MLEM.Ui/Elements/Group.cs
@@ -0,0 +1,10 @@
+using Microsoft.Xna.Framework;
+
+namespace MLEM.Ui.Elements {
+    public class Group : Element {
+
+        public Group(Anchor anchor, Vector2 size) : base(anchor, size) {
+        }
+
+    }
+}
