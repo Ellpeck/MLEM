83c45ed02d5f119dd901e577e1fcdf5ae61ff7a4
Ellpeck <megamaximal@gmail.com>
2019-08-13 16:05:18 +0200

    made the group component set height based on children by default

--- MLEM.Ui/Elements/Group.cs
+++ MLEM.Ui/Elements/Group.cs
@@ -3,7 +3,8 @@ using Microsoft.Xna.Framework;
 namespace MLEM.Ui.Elements {
     public class Group : Element {
 
-        public Group(Anchor anchor, Vector2 size) : base(anchor, size) {
+        public Group(Anchor anchor, Vector2 size, bool setHeightBasedOnChildren = true) : base(anchor, size) {
+            this.SetHeightBasedOnChildren = setHeightBasedOnChildren;
             this.IgnoresMouse = true;
         }
 
