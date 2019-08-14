6ede030138dcb7b2c9dd9f84bd20fe058f69f87c
Ellpeck <megamaximal@gmail.com>
2019-08-14 22:06:06 +0200

    make image texture not read only

--- MLEM.Ui/Elements/Image.cs
+++ MLEM.Ui/Elements/Image.cs
@@ -7,20 +7,20 @@ namespace MLEM.Ui.Elements {
     public class Image : Element {
 
         public Color Color = Color.White;
-        private readonly TextureRegion texture;
+        public TextureRegion Texture;
         private readonly bool scaleToImage;
 
         public Image(Anchor anchor, Vector2 size, TextureRegion texture, bool scaleToImage = false) : base(anchor, size) {
-            this.texture = texture;
+            this.Texture = texture;
             this.scaleToImage = scaleToImage;
         }
 
         protected override Point CalcActualSize(Rectangle parentArea) {
-            return this.scaleToImage ? this.texture.Size : base.CalcActualSize(parentArea);
+            return this.scaleToImage ? this.Texture.Size : base.CalcActualSize(parentArea);
         }
 
         public override void Draw(GameTime time, SpriteBatch batch, float alpha, Point offset) {
-            batch.Draw(this.texture, this.DisplayArea.OffsetCopy(offset), this.Color * alpha);
+            batch.Draw(this.Texture, this.DisplayArea.OffsetCopy(offset), this.Color * alpha);
             base.Draw(time, batch, alpha, offset);
         }
 
