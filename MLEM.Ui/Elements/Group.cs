8d648cd5dce082d4c8e7a7d306dea4b64b8c67fb
Ellpeck <megamaximal@gmail.com>
2019-08-15 13:26:40 +0200

    make groups not ignore mouse by default :V

--- MLEM.Ui/Elements/Group.cs
+++ MLEM.Ui/Elements/Group.cs
@@ -5,7 +5,6 @@ namespace MLEM.Ui.Elements {
 
         public Group(Anchor anchor, Vector2 size, bool setHeightBasedOnChildren = true) : base(anchor, size) {
             this.SetHeightBasedOnChildren = setHeightBasedOnChildren;
-            this.IgnoresMouse = true;
         }
 
     }
