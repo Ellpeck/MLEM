f0b144001139250a6932ac1c25921f4de690f6a1
Ellpeck <megamaximal@gmail.com>
2019-09-01 11:56:12 +0200

    git add pls

---
+++ Demos.DesktopGL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demos.DesktopGL {
+    public static class Program {
+
+        public static void Main() {
+            using (var game = new GameImpl())
+                game.Run();
+        }
+
+    }
+}
