1ee7fafc238bf15b11e0a3076d75e4fa4b2d1e3b
Ellpeck <megamaximal@gmail.com>
2019-09-04 16:37:57 +0200

    change this to ceil so that elements don't just get cut off

--- MLEM.Ui/Elements/Panel.cs
+++ MLEM.Ui/Elements/Panel.cs
@@ -72,7 +72,7 @@ namespace MLEM.Ui.Elements {
                 var lowestChild = this.GetLowestChild(false, true);
                 // the max value of the scrollbar is the amount of non-scaled pixels taken up by overflowing components
                 var childrenHeight = lowestChild.Area.Bottom - firstChild.Area.Top;
-                this.ScrollBar.MaxValue = ((childrenHeight - this.Area.Height) / this.Scale + this.ChildPadding.Y * 2).Floor();
+                this.ScrollBar.MaxValue = ((childrenHeight - this.Area.Height) / this.Scale + this.ChildPadding.Y * 2).Ceil();
 
                 // update the render target
                 var targetArea = this.GetRenderTargetArea();
