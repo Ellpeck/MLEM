d224d57cf2e4d677b673b317b3cc83d83f51d1c6
Ellpeck <megamaximal@gmail.com>
2019-12-01 22:58:20 +0100

    added a handler for this

--- MLEM/Extensions/GraphicsExtensions.cs
+++ MLEM/Extensions/GraphicsExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -17,6 +18,9 @@ namespace MLEM.Extensions {
                 manager.PreferredBackBufferWidth = curr.Width;
                 manager.PreferredBackBufferHeight = curr.Height;
             } else {
+                if (lastWidth <= 0 || lastHeight <= 0)
+                    throw new InvalidOperationException("Can't call SetFullscreen to change out of fullscreen mode without going into fullscreen mode first");
+
                 manager.PreferredBackBufferWidth = lastWidth;
                 manager.PreferredBackBufferHeight = lastHeight;
             }
