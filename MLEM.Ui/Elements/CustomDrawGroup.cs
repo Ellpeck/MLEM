da603c5f05cdc9868dcdb56c59fa79c6ebbc9c59
Ellpeck <megamaximal@gmail.com>
2019-12-29 19:28:56 +0100

    made custom draw groups be a bit easier to use

--- MLEM.Ui/Elements/CustomDrawGroup.cs
+++ MLEM.Ui/Elements/CustomDrawGroup.cs
@@ -4,21 +4,21 @@ using Microsoft.Xna.Framework.Graphics;
 namespace MLEM.Ui.Elements {
     public class CustomDrawGroup : Group {
 
-        public Matrix Transform;
+        public Matrix? Transform;
+        public TransformCallback TransformGetter;
         public BeginDelegate BeginImpl;
 
-        public CustomDrawGroup(Anchor anchor, Vector2 size, Matrix? transform = null, BeginDelegate beginImpl = null, bool setHeightBasedOnChildren = true) :
+        public CustomDrawGroup(Anchor anchor, Vector2 size, TransformCallback transformGetter = null, BeginDelegate beginImpl = null, bool setHeightBasedOnChildren = true) :
             base(anchor, size, setHeightBasedOnChildren) {
-            this.Transform = transform ?? Matrix.Identity;
+            this.TransformGetter = transformGetter ?? ((element, time, matrix) => Matrix.Identity);
             this.BeginImpl = beginImpl ?? ((element, time, batch, alpha, blendState, samplerState, matrix) => batch.Begin(SpriteSortMode.Deferred, blendState, samplerState, null, null, null, matrix));
         }
-
-        public delegate void BeginDelegate(CustomDrawGroup element, GameTime time, SpriteBatch batch, float alpha, BlendState blendState, SamplerState samplerState, Matrix matrix);
 
         public override void Draw(GameTime time, SpriteBatch batch, float alpha, BlendState blendState, SamplerState samplerState, Matrix matrix) {
             // end the usual draw so that we can begin our own
             batch.End();
-            var mat = matrix * this.Transform;
+            var trans = this.Transform ?? this.TransformGetter(this, time, matrix);
+            var mat = matrix * trans;
             this.BeginImpl(this, time, batch, alpha, blendState, samplerState, mat);
             // draw child components in custom begin call
             base.Draw(time, batch, alpha, blendState, samplerState, mat);
@@ -27,6 +27,10 @@ namespace MLEM.Ui.Elements {
             // begin the usual draw again for other elements
             batch.Begin(SpriteSortMode.Deferred, blendState, samplerState, null, null, null, matrix);
         }
+
+        public delegate void BeginDelegate(CustomDrawGroup element, GameTime time, SpriteBatch batch, float alpha, BlendState blendState, SamplerState samplerState, Matrix matrix);
+
+        public delegate Matrix TransformCallback(CustomDrawGroup element, GameTime time, Matrix matrix);
 
     }
 }
