7ca8256b777f4085df7ce98f7d6f3d5581a2a132
Ellpeck <megamaximal@gmail.com>
2020-01-17 01:33:31 +0100

    added a weight-based list randomness function

--- MLEM/Extensions/RandomExtensions.cs
+++ MLEM/Extensions/RandomExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MLEM.Extensions {
     public static class RandomExtensions {
@@ -10,6 +11,18 @@ namespace MLEM.Extensions {
 
         public static T GetRandomEntry<T>(this Random random, IList<T> entries) {
             return entries[random.Next(entries.Count)];
+        }
+
+        public static T GetRandomWeightedEntry<T>(this Random random, IList<T> entries, Func<T, int> weightFunc) {
+            var totalWeight = entries.Sum(weightFunc);
+            var goalWeight = random.Next(totalWeight);
+            var currWeight = 0;
+            foreach (var entry in entries) {
+                currWeight += weightFunc(entry);
+                if (currWeight >= goalWeight)
+                    return entry;
+            }
+            throw new IndexOutOfRangeException();
         }
 
     }
