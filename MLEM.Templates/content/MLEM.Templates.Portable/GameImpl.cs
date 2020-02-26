052b83bbf59720f99b1a7c4fc39698d9d8e4cfc3
Ellpeck <megamaximal@gmail.com>
2020-02-26 13:24:42 +0100

    added instances to templates

--- MLEM.Templates/content/MLEM.Templates.Portable/GameImpl.cs
+++ MLEM.Templates/content/MLEM.Templates.Portable/GameImpl.cs
@@ -3,5 +3,11 @@ using MLEM.Startup;
 namespace TemplateNamespace {
     public class GameImpl : MlemGame {
 
+        public static GameImpl Instance { get; private set; }
+
+        public GameImpl() {
+            Instance = this;
+        }
+        
     }
 }
