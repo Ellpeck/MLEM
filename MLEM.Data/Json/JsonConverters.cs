2744f7d8063aac4bb5c2b232abed31d2b73df984
Ellpeck <megamaximal@gmail.com>
2020-02-27 19:43:36 +0100

    made json converter collection code a bit cleaner

--- MLEM.Data/Json/JsonConverters.cs
+++ MLEM.Data/Json/JsonConverters.cs
@@ -6,8 +6,8 @@ using Newtonsoft.Json;
 namespace MLEM.Data.Json {
     public class JsonConverters {
 
-        public static readonly JsonConverter[] Converters = Assembly.GetExecutingAssembly().GetExportedTypes()
-            .Where(t => t.Namespace == typeof(JsonConverters).Namespace && t.IsSubclassOf(typeof(JsonConverter)))
+        public static readonly JsonConverter[] Converters = typeof(JsonConverters).Assembly.GetExportedTypes()
+            .Where(t => t.IsSubclassOf(typeof(JsonConverter)))
             .Select(t => t.GetConstructor(Type.EmptyTypes).Invoke(null)).Cast<JsonConverter>().ToArray();
 
         public static JsonSerializer AddAll(JsonSerializer serializer) {
