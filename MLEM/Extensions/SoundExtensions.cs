5404c36f7389902ddbeb208bf322a2f3a9ddf147
Ellpeck <megamaximal@gmail.com>
2020-04-11 15:15:04 +0200

    mark replay as obsolete

--- MLEM/Extensions/SoundExtensions.cs
+++ MLEM/Extensions/SoundExtensions.cs
@@ -1,8 +1,10 @@
+using System;
 using Microsoft.Xna.Framework.Audio;
 
 namespace MLEM.Extensions {
     public static class SoundExtensions {
 
+        [Obsolete("When using the .NET Core version of MonoGame, the replay issue has been fixed. Just call Play() instead.")]
         public static void Replay(this SoundEffectInstance sound) {
             sound.Stop();
             sound.Play();
