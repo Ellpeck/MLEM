3e0fac9a141a2df8cb4373cea4f331dba5d56b22
Ellpeck <megamaximal@gmail.com>
2020-07-31 17:25:56 +0200

    Properly allow tests to use content files

--- Tests/Stub/StubContent.cs
+++ Tests/Stub/StubContent.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using System.IO;
 using Microsoft.Xna.Framework.Content;
+using NUnit.Framework;
 
 namespace Tests.Stub {
     public class StubContent : ContentManager {
@@ -7,6 +9,7 @@ namespace Tests.Stub {
         private readonly Dictionary<string, object> assets;
 
         public StubContent(Dictionary<string, object> assets) : base(new StubServices()) {
+            this.RootDirectory = Path.Combine(TestContext.CurrentContext.TestDirectory, "Content");
             this.assets = assets;
         }
 
--- Tests/TestDataTextureAtlas.cs
+++ Tests/TestDataTextureAtlas.cs
@@ -12,30 +12,10 @@ namespace Tests {
 
         [Test]
         public void Test() {
-            const string data = @"
-SimpleDeskUp
-loc 0 0 48 32
-piv 16 16
-SimpleDeskRight
-loc 48 0 48 32
-piv 80 16
-
-Plant
-loc 96 0 16 32
-
-LongTableUp
-loc 0 32 64 48
-piv 16 48
-LongTableRight
-loc 64 32 64 48
-piv 112 48";
-            using (var file = new FileInfo("texture.atlas").CreateText())
-                file.Write(data);
-
             var content = new StubContent(new Dictionary<string, object> {
-                {"texture", new Texture2D(new StubGraphics(), 1, 1)}
+                {"Texture", new Texture2D(new StubGraphics(), 1, 1)}
             });
-            var atlas = content.LoadTextureAtlas("texture");
+            var atlas = content.LoadTextureAtlas("Texture");
             Assert.AreEqual(atlas.Regions.Count(), 5);
 
             var table = atlas["LongTableUp"];
