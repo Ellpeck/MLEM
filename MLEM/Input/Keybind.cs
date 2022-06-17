aabb1ed5df447279a0c05bcf3bfb31a33f20f3dd
Ellpeck <me@ellpeck.de>
2022-06-17 18:23:47 +0200

    added .editorconfig

--- MLEM/Input/Keybind.cs
+++ MLEM/Input/Keybind.cs
@@ -217,7 +217,7 @@ namespace MLEM.Input {
         /// <summary>Compares the current instance with another object of the same type and returns an integer that indicates whether the current instance precedes, follows, or occurs in the same position in the sort order as the other object.</summary>
         /// <param name="other">An object to compare with this instance.</param>
         /// <returns>A value that indicates the relative order of the objects being compared. The return value has these meanings:
-        /// 
+        ///
         /// <list type="table"><listheader><term> Value</term><description> Meaning</description></listheader><item><term> Less than zero</term><description> This instance precedes <paramref name="other" /> in the sort order.</description></item><item><term> Zero</term><description> This instance occurs in the same position in the sort order as <paramref name="other" />.</description></item><item><term> Greater than zero</term><description> This instance follows <paramref name="other" /> in the sort order.</description></item></list></returns>
         public int CompareTo(Keybind other) {
             return this.combinations.Sum(c => other.combinations.Sum(c.CompareTo));
@@ -228,7 +228,7 @@ namespace MLEM.Input {
         /// <exception cref="T:System.ArgumentException">
         /// <paramref name="obj" /> is not the same type as this instance.</exception>
         /// <returns>A value that indicates the relative order of the objects being compared. The return value has these meanings:
-        /// 
+        ///
         /// <list type="table"><listheader><term> Value</term><description> Meaning</description></listheader><item><term> Less than zero</term><description> This instance precedes <paramref name="obj" /> in the sort order.</description></item><item><term> Zero</term><description> This instance occurs in the same position in the sort order as <paramref name="obj" />.</description></item><item><term> Greater than zero</term><description> This instance follows <paramref name="obj" /> in the sort order.</description></item></list></returns>
         public int CompareTo(object obj) {
             if (object.ReferenceEquals(null, obj))
@@ -355,7 +355,7 @@ namespace MLEM.Input {
             /// <summary>Compares the current instance with another object of the same type and returns an integer that indicates whether the current instance precedes, follows, or occurs in the same position in the sort order as the other object.</summary>
             /// <param name="other">An object to compare with this instance.</param>
             /// <returns>A value that indicates the relative order of the objects being compared. The return value has these meanings:
-            /// 
+            ///
             /// <list type="table"><listheader><term> Value</term><description> Meaning</description></listheader><item><term> Less than zero</term><description> This instance precedes <paramref name="other" /> in the sort order.</description></item><item><term> Zero</term><description> This instance occurs in the same position in the sort order as <paramref name="other" />.</description></item><item><term> Greater than zero</term><description> This instance follows <paramref name="other" /> in the sort order.</description></item></list></returns>
             public int CompareTo(Combination other) {
                 return this.Modifiers.Length.CompareTo(other.Modifiers.Length);
@@ -366,7 +366,7 @@ namespace MLEM.Input {
             /// <exception cref="T:System.ArgumentException">
             /// <paramref name="obj" /> is not the same type as this instance.</exception>
             /// <returns>A value that indicates the relative order of the objects being compared. The return value has these meanings:
-            /// 
+            ///
             /// <list type="table"><listheader><term> Value</term><description> Meaning</description></listheader><item><term> Less than zero</term><description> This instance precedes <paramref name="obj" /> in the sort order.</description></item><item><term> Zero</term><description> This instance occurs in the same position in the sort order as <paramref name="obj" />.</description></item><item><term> Greater than zero</term><description> This instance follows <paramref name="obj" /> in the sort order.</description></item></list></returns>
             public int CompareTo(object obj) {
                 if (object.ReferenceEquals(null, obj))
--- Sandbox/Content/Tiled/Tileset.tsx
+++ Sandbox/Content/Tiled/Tileset.tsx
@@ -1,123 +1,123 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.1" name="TownTiles" tilewidth="16" tileheight="16" tilecount="512" columns="32">
- <image source="Tiles.png" width="512" height="256"/>
- <tile id="3">
-  <properties>
-   <property name="Walkability" type="int" value="30"/>
-  </properties>
- </tile>
- <tile id="4">
-  <properties>
-   <property name="Walkability" type="int" value="30"/>
-  </properties>
- </tile>
- <tile id="5">
-  <properties>
-   <property name="Walkability" type="int" value="30"/>
-  </properties>
- </tile>
- <tile id="7">
-  <objectgroup draworder="index" id="5">
-   <object id="4" x="8" y="3" width="5" height="10"/>
-  </objectgroup>
- </tile>
- <tile id="8">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="3" y="2" width="10" height="5"/>
-  </objectgroup>
- </tile>
- <tile id="15">
-  <animation>
-   <frame tileid="15" duration="1000"/>
-   <frame tileid="47" duration="1000"/>
-  </animation>
- </tile>
- <tile id="33">
-  <properties>
-   <property name="Walkability" type="int" value="30"/>
-  </properties>
- </tile>
- <tile id="34">
-  <properties>
-   <property name="Walkability" type="int" value="30"/>
-  </properties>
- </tile>
- <tile id="35">
-  <properties>
-   <property name="Walkability" type="int" value="30"/>
-  </properties>
- </tile>
- <tile id="36">
-  <properties>
-   <property name="Walkability" type="int" value="30"/>
-  </properties>
- </tile>
- <tile id="37">
-  <properties>
-   <property name="Walkability" type="int" value="30"/>
-  </properties>
- </tile>
- <tile id="65">
-  <properties>
-   <property name="Walkability" type="int" value="30"/>
-  </properties>
- </tile>
- <tile id="66">
-  <properties>
-   <property name="Walkability" type="int" value="30"/>
-  </properties>
- </tile>
- <tile id="67">
-  <properties>
-   <property name="Walkability" type="int" value="30"/>
-  </properties>
- </tile>
- <tile id="68">
-  <properties>
-   <property name="Walkability" type="int" value="30"/>
-  </properties>
- </tile>
- <tile id="69">
-  <properties>
-   <property name="Walkability" type="int" value="30"/>
-  </properties>
- </tile>
- <tile id="72">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="12" y="0" width="4" height="4"/>
-  </objectgroup>
- </tile>
- <tile id="132">
-  <properties>
-   <property name="Walkability" type="int" value="60"/>
-  </properties>
- </tile>
- <tile id="172">
-  <properties>
-   <property name="LightColor" type="color" value="#ffaa6300"/>
-   <property name="LightRadius" type="float" value="5.5"/>
-   <property name="LightType" value="Lamp"/>
-  </properties>
- </tile>
- <tile id="289">
-  <properties>
-   <property name="LightColor" type="color" value="#ff676e73"/>
-   <property name="LightRadius" type="float" value="1.5"/>
-   <property name="LightType" value="Window"/>
-  </properties>
- </tile>
- <tile id="353">
-  <properties>
-   <property name="LightColor" type="color" value="#ff676e73"/>
-   <property name="LightRadius" type="float" value="2.5"/>
-   <property name="LightType" value="Window"/>
-  </properties>
- </tile>
- <tile id="385">
-  <properties>
-   <property name="LightColor" type="color" value="#ff676e73"/>
-   <property name="LightRadius" type="float" value="2.5"/>
-   <property name="LightType" value="Window"/>
-  </properties>
- </tile>
+    <image source="Tiles.png" width="512" height="256" />
+    <tile id="3">
+        <properties>
+            <property name="Walkability" type="int" value="30" />
+        </properties>
+    </tile>
+    <tile id="4">
+        <properties>
+            <property name="Walkability" type="int" value="30" />
+        </properties>
+    </tile>
+    <tile id="5">
+        <properties>
+            <property name="Walkability" type="int" value="30" />
+        </properties>
+    </tile>
+    <tile id="7">
+        <objectgroup draworder="index" id="5">
+            <object id="4" x="8" y="3" width="5" height="10" />
+        </objectgroup>
+    </tile>
+    <tile id="8">
+        <objectgroup draworder="index" id="2">
+            <object id="1" x="3" y="2" width="10" height="5" />
+        </objectgroup>
+    </tile>
+    <tile id="15">
+        <animation>
+            <frame tileid="15" duration="1000" />
+            <frame tileid="47" duration="1000" />
+        </animation>
+    </tile>
+    <tile id="33">
+        <properties>
+            <property name="Walkability" type="int" value="30" />
+        </properties>
+    </tile>
+    <tile id="34">
+        <properties>
+            <property name="Walkability" type="int" value="30" />
+        </properties>
+    </tile>
+    <tile id="35">
+        <properties>
+            <property name="Walkability" type="int" value="30" />
+        </properties>
+    </tile>
+    <tile id="36">
+        <properties>
+            <property name="Walkability" type="int" value="30" />
+        </properties>
+    </tile>
+    <tile id="37">
+        <properties>
+            <property name="Walkability" type="int" value="30" />
+        </properties>
+    </tile>
+    <tile id="65">
+        <properties>
+            <property name="Walkability" type="int" value="30" />
+        </properties>
+    </tile>
+    <tile id="66">
+        <properties>
+            <property name="Walkability" type="int" value="30" />
+        </properties>
+    </tile>
+    <tile id="67">
+        <properties>
+            <property name="Walkability" type="int" value="30" />
+        </properties>
+    </tile>
+    <tile id="68">
+        <properties>
+            <property name="Walkability" type="int" value="30" />
+        </properties>
+    </tile>
+    <tile id="69">
+        <properties>
+            <property name="Walkability" type="int" value="30" />
+        </properties>
+    </tile>
+    <tile id="72">
+        <objectgroup draworder="index" id="2">
+            <object id="1" x="12" y="0" width="4" height="4" />
+        </objectgroup>
+    </tile>
+    <tile id="132">
+        <properties>
+            <property name="Walkability" type="int" value="60" />
+        </properties>
+    </tile>
+    <tile id="172">
+        <properties>
+            <property name="LightColor" type="color" value="#ffaa6300" />
+            <property name="LightRadius" type="float" value="5.5" />
+            <property name="LightType" value="Lamp" />
+        </properties>
+    </tile>
+    <tile id="289">
+        <properties>
+            <property name="LightColor" type="color" value="#ff676e73" />
+            <property name="LightRadius" type="float" value="1.5" />
+            <property name="LightType" value="Window" />
+        </properties>
+    </tile>
+    <tile id="353">
+        <properties>
+            <property name="LightColor" type="color" value="#ff676e73" />
+            <property name="LightRadius" type="float" value="2.5" />
+            <property name="LightType" value="Window" />
+        </properties>
+    </tile>
+    <tile id="385">
+        <properties>
+            <property name="LightColor" type="color" value="#ff676e73" />
+            <property name="LightRadius" type="float" value="2.5" />
+            <property name="LightType" value="Window" />
+        </properties>
+    </tile>
 </tileset>
