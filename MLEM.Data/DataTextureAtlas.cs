e673ccea615cdf3fa3724175ec5e2f9fe7ee060a
Ellpeck <me@ellpeck.de>
2022-07-18 20:46:59 +0200

    fixed a missing using directive in MLEM.FNA

--- MLEM.Data/DataTextureAtlas.cs
+++ MLEM.Data/DataTextureAtlas.cs
@@ -6,6 +6,7 @@ using System.Text.RegularExpressions;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using MLEM.Extensions;
 using MLEM.Textures;
 
 namespace MLEM.Data {
