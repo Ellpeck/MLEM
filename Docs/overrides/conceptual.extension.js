1d4a2ebdf7f5f539a634ce0868a8ac636a4ba5b0
Ellpeck <me@ellpeck.de>
2023-05-10 23:06:34 +0200

    reduce header levels in README to allow for TOC navigation on the docs site

--- Docs/overrides/conceptual.extension.js
+++ Docs/overrides/conceptual.extension.js
@@ -1,6 +1,11 @@
 exports.preTransform = function (model) {
-    // point to the release branch in the readme when on the website
-    if (model._path.includes("index"))
+    if (model._path.includes("index")) {
+        // point to the release branch in the readme
         model.conceptual = model.conceptual.replaceAll(/\/MLEM(\/[^/]+)?\/main\//g, "/MLEM$1/release/");
+
+        // reduce header levels by 1 to allow for TOC navigation
+        for (let i = 5; i >= 1; i--)
+            model.conceptual = model.conceptual.replaceAll(`<h${i}`, `<h${i + 1}`).replaceAll(`</h${i}`, `</h${i + 1}`);
+    }
     return model;
 };
