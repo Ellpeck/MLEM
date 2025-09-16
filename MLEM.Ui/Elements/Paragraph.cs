665add85e06aa8a8a36c45ace6c6d041adb7fee6
Ellpeck <me@ellpeck.de>
2025-09-17 00:02:11 +0200

    Move CheckTextChange to ForceUpdateArea

--- MLEM.Ui/Elements/Paragraph.cs
+++ MLEM.Ui/Elements/Paragraph.cs
@@ -209,9 +209,11 @@ namespace MLEM.Ui.Elements {
         /// <param name="width">The paragraph's width. Note that its height is automatically calculated.</param>
         /// <param name="textCallback">The paragraph's text</param>
         /// <param name="autoAdjustWidth">Whether the paragraph's width should automatically be calculated based on the text within it.</param>
-        public Paragraph(Anchor anchor, float width, TextCallback textCallback, bool autoAdjustWidth = false) : this(anchor, width, string.Empty, autoAdjustWidth) {
+        public Paragraph(Anchor anchor, float width, TextCallback textCallback, bool autoAdjustWidth = false) : base(anchor, new Vector2(width, 0)) {
             this.GetTextCallback = textCallback;
-            this.CheckTextChange();
+            this.AutoAdjustWidth = autoAdjustWidth;
+            this.CanBeSelected = false;
+            this.CanBeMoused = false;
         }
 
         /// <summary>
@@ -236,9 +238,14 @@ namespace MLEM.Ui.Elements {
         }
 
         /// <inheritdoc />
+        public override void ForceUpdateArea() {
+            this.CheckTextChange();
+            base.ForceUpdateArea();
+        }
+
+        /// <inheritdoc />
         protected override Vector2 CalcActualSize(RectangleF parentArea) {
             var size = base.CalcActualSize(parentArea);
-            this.CheckTextChange();
             this.TokenizeIfNecessary();
             this.AlignAndSplitIfNecessary(size);
             var textSize = this.tokenizedText.GetArea(scale: this.TextScale * this.TextScaleMultiplier * this.Scale).Size;
@@ -281,7 +288,7 @@ namespace MLEM.Ui.Elements {
         }
 
         private void CheckTextChange() {
-            var newText = this.GetTextCallback?.Invoke(this) ?? this.explicitlySetText;
+            var newText = this.GetTextCallback?.Invoke(this) ?? this.explicitlySetText ?? string.Empty;
             if (this.displayedText == newText)
                 return;
             var emptyChanged = string.IsNullOrWhiteSpace(this.displayedText) != string.IsNullOrWhiteSpace(newText);
