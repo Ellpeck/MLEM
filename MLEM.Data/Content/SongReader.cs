e38636b639f39a9d44b55640b3531262f6230c06
Ellpeck <me@ellpeck.de>
2026-07-28 19:00:25 +0200

    throw more cleanly when trying to read a song from a non-rooted path

--- MLEM.Data/Content/SongReader.cs
+++ MLEM.Data/Content/SongReader.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Media;
 
 namespace MLEM.Data.Content {
@@ -8,6 +9,8 @@ namespace MLEM.Data.Content {
 
         /// <inheritdoc />
         protected override Song Read(RawContentManager manager, string assetPath, Stream stream, Song existing) {
+            if (!Path.IsPathRooted(assetPath))
+                throw new ContentLoadException($"Cannot read song from non-rooted path {assetPath}");
             return Song.FromUri(Path.GetFileNameWithoutExtension(assetPath), new Uri(assetPath));
         }
 
